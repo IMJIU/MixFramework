cc5e120ad50e6853cd438875c4835ad66c48be9e
zlfkyo <zlfkyo@sina.com>
2016-07-01 17:26:45 +0800

    commit

--- src/main/java/com/framework/json/MyGroupSetUser.java
+++ src/main/java/com/framework/json/MyGroupSetUser.java
@@ -5,34 +5,34 @@ import java.util.Set;
 
 public class MyGroupSetUser {
 
-private Long groupsetid;
-private String groupsetname;
-private Set<MyUser> setuser = new HashSet<MyUser>();
+	private Long groupsetid;
+	private String groupsetname;
+	private Set<MyUser> setuser = new HashSet<MyUser>();
 
-public MyGroupSetUser() {
-}
+	public MyGroupSetUser() {
+	}
 
-public Long getGroupsetid() {
-return groupsetid;
-}
+	public Long getGroupsetid() {
+		return groupsetid;
+	}
 
-public void setGroupsetid(Long groupsetid) {
-this.groupsetid = groupsetid;
-}
+	public void setGroupsetid(Long groupsetid) {
+		this.groupsetid = groupsetid;
+	}
 
-public String getGroupsetname() {
-return groupsetname;
-}
+	public String getGroupsetname() {
+		return groupsetname;
+	}
 
-public void setGroupsetname(String groupsetname) {
-this.groupsetname = groupsetname;
-}
+	public void setGroupsetname(String groupsetname) {
+		this.groupsetname = groupsetname;
+	}
 
-public Set<MyUser> getSetuser() {
-return setuser;
-}
+	public Set<MyUser> getSetuser() {
+		return setuser;
+	}
 
-public void setSetuser(Set<MyUser> setuser) {
-this.setuser = setuser;
-}
+	public void setSetuser(Set<MyUser> setuser) {
+		this.setuser = setuser;
+	}
 }
--- src/main/java/com/util/upload_to_linux/T5_upload_to_linux.java
+++ src/main/java/com/util/upload_to_linux/T5_upload_to_linux.java
@@ -1,35 +1,81 @@
 package com.util.upload_to_linux;
 
+import org.junit.Test;
+
 import com.util.upload_to_linux.base.*;
 
 public class T5_upload_to_linux {
 
 	public static void main(String[] args) throws Exception {
-		//配置
+		// 配置
 		Base.server = ".net";
 		Base.findType = "class";// class | html
-		
-		/**    【移动文件 】   */
-		 FindOperator.processFile((s) -> MoveOperate.doMoveClass(s));
-		
-		/**     【上传class】    */
-//		FindOperator.processFile((s) -> UploadOperate.doUploadClass(s));
-		 
-		/**    【扫描需要上传的html】   */
-//		FindOperator.processFile((s) -> UploadOperate.findUploadHtmlWriteToFile(s));
-		 
-		/** 	【上传html文件】		*/
-//		UploadOperate.doUploadHtmlFileListToLinux();
-		 
-		
-		/**		【下载jar】	*/
-//		DownloadOperator.download(Base.linux_lib_Path, Base.dao_jar, Base.moveToDir);
-//		DownloadOperator.download(Base.linux_lib_Path, Base.service_jar, Base.moveToDir);
-		
-		/**		【上传jar】 	*/
-//		UploadOperate.uploadToLinux(Base.linux_lib_Path, Base.moveToDir + Base.dao_jar);
-//		UploadOperate.uploadToLinux(Base.linux_lib_Path, Base.moveToDir + Base.service_jar);
-//		UploadOperate.uploadWarToLinux(Base.linux_idongri_Path,  Base.local_platform_war_path);
+
+		/** 【移动文件 】 */
+		FindOperator.processFile((s) -> MoveOperate.doMoveClass(s));
+
+		/** 【上传class】 */
+		// FindOperator.processFile((s) -> UploadOperate.doUploadClass(s));
+
+		/** 【扫描需要上传的html】 */
+		// FindOperator.processFile((s) ->
+		// UploadOperate.findUploadHtmlWriteToFile(s));
+
+		/** 【上传html文件】 */
+		// UploadOperate.doUploadHtmlFileListToLinux();
+
+		/** 【下载jar】 */
+		// DownloadOperator.download(Base.linux_lib_Path, Base.dao_jar,
+		// Base.moveToDir);
+		// DownloadOperator.download(Base.linux_lib_Path, Base.service_jar,
+		// Base.moveToDir);
+
+		/** 【上传jar】 */
+		// UploadOperate.uploadToLinux(Base.linux_lib_Path, Base.moveToDir +
+		// Base.dao_jar);
+		// UploadOperate.uploadToLinux(Base.linux_lib_Path, Base.moveToDir +
+		// Base.service_jar);
+		// UploadOperate.uploadWarToLinux(Base.linux_idongri_Path,
+		// Base.local_platform_war_path);
 	}
 
+	/** 【移动文件 】 */
+	@Test
+	public void t1_move() throws Exception {
+		FindOperator.processFile((s) -> MoveOperate.doMoveClass(s));
+	}
+
+	/** 【上传class】 */
+	@Test
+	public void t2_upload_class() throws Exception {
+		FindOperator.processFile((s) -> UploadOperate.doUploadClass(s));
+	}
+
+	/** 【扫描需要上传的html】 */
+	@Test
+	public void t3_scan_html() throws Exception {
+		FindOperator.processFile((s) -> UploadOperate.findUploadHtmlWriteToFile(s));
+	}
+
+	/** 【上传html文件】 */
+	@Test
+	public void t4_upload_html() throws Exception {
+		UploadOperate.doUploadHtmlFileListToLinux();
+	}
+
+	/** 【下载jar】 */
+	@Test
+	public void t5_download() throws Exception {
+		DownloadOperator.download(Base.linux_lib_Path, Base.dao_jar, Base.moveToDir);
+		DownloadOperator.download(Base.linux_lib_Path, Base.service_jar, Base.moveToDir);
+	}
+
+	/** 【上传jar】 */
+	@Test
+	public void t6_upload() throws Exception {
+		UploadOperate.uploadToLinux(Base.linux_lib_Path, Base.moveToDir + Base.dao_jar);
+		UploadOperate.uploadToLinux(Base.linux_lib_Path, Base.moveToDir + Base.service_jar);
+		// UploadOperate.uploadWarToLinux(Base.linux_idongri_Path,
+		// Base.local_platform_war_path);
+	}
 }
